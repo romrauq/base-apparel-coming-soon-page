34b58d1946bd4f0eec765f0ae6dcc338ada4288e
romrauq <johnequarmor@gmail.com>
2025-01-07 20:03:27 +0000

    Set input field border color to change when email provide is valid/invalid.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,6 +1,7 @@
 // Get references to the input field, alert text, and image button
 const emailInput = document.getElementById("input-field");
 const alertText = document.getElementById("alert");
+const inputField = document.getElementById("input-field");
 const inputImageDiv = document.querySelector(".input-image-div");
 
 // Hide the alert text by default
@@ -17,8 +18,10 @@ inputImageDiv.addEventListener("click", () => {
 	const emailValue = emailInput.value.trim();
 
 	if (validateEmail(emailValue)) {
+		inputField.style.border = "1px solid hsla(0, 6%, 24%, 0.3)"; //Change input field border color.
 		alertText.style.display = "none"; // Hide alert if the email is valid
 	} else {
+		inputField.style.border = "2px solid hsl(0, 93%, 68%)"; //Change input field border color.
 		alertText.style.display = "block"; // Show alert if the email is invalid
 	}
 });
